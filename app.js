ee27eaea84445b27f1ba6920da3da5ff4185ed18
sudokidn2h112 <sudokidn2h112@users.noreply.github.com>
2016-10-07 14:07:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,26 @@ var userRoutes = require('./routes/user');
 
 var app = express();
 
-mongoose.connect('localhost:27017/shopping');
+//add to deploy
+    // Here we find an appropriate database to connect to, defaulting to
+    // localhost if we don't find one.
+    var uristring =
+    'mongodb://sudokid:langtutm1411@ds053206.mlab.com:53206/sudokid' || 'mongodb://localhost:27017/shopping';
+
+    // The http server will listen to an appropriate port, or default to
+
+
+    // Makes connection asynchronously.  Mongoose will queue up database
+    // operations and release them when the connection is complete.
+    mongoose.connect(uristring, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
+
+//mongoose.connect('localhost:27017/shopping');
 
 require('./config/passport');
 
